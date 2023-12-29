f898edb67a421c93c516e0a5817ba2e2c7ae2eb3
mealton <titov_yw@mail.ru>
2023-12-29 10:22:01 +0300

    гифки

--- engine/controllers/profile.controller.php
+++ engine/controllers/profile.controller.php
@@ -12,6 +12,7 @@ class Profile extends Main
 
     public function __construct($query = [], $async = false)
     {
+        $this->components['extra-vendors'] = ['giffer' => 'javascript-giffer'];
         $this->uploader = render('components', 'uploader', ['upload_folder' => 'img/profile/', 'multiple' => 0, 'placeholder' => 'Изображение пользователя']);
         parent::__construct($query, $async);
     }
